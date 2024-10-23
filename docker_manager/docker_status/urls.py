7b72082862bcc426fa0699cdb9d4ed4432ae0d0e
Mohamadreza <175953983+mrg5009@users.noreply.github.com>
2024-10-23 06:11:01 +0000

    Done

--- docker_manager/docker_status/urls.py
+++ docker_manager/docker_status/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import get_docker_status
+from .views import get_docker_status  
 
-
-urlpatters = [path('status/', get_docker_status,name='docker_status')]
-
+urlpatterns = [
+    path('status/', get_docker_status, name='docker_status'),
+]
