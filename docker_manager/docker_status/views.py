ee4b5538f2f863d29677090b8a33c6abbc5a46ac
Mohamadreza <175953983+mrg5009@users.noreply.github.com>
2024-10-23 05:52:20 +0000

    update views.py

---
+++ docker_manager/docker_status/views.py
@@ -0,0 +1,24 @@
+import paramiko 
+from  requests import request
+from django.shortcuts import rander
+
+def get_docker_status():
+    
+    hostname = "31.15.17.210"
+    username = "apsiloon"
+    password = "09224976231"
+
+    try:
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddpolicy())
+        client.connect(hostname ,username=username , password=password)
+
+        stdin , stdout , stderr = client.exec_command('docker ps')
+        docker_ps_output = stdout.read().decode("uft-8")
+
+        client.close()
+
+    except Exception as e:
+        docker_ps_output = f"Error: {str(e)}"
+
+    return rander(request, 'docker_status/status.html',{"docker_ps_output" : docker_ps_output})
